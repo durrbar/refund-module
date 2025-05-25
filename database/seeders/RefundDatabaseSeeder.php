fdca8a362ae3b8425110aaba821a1c80102f1925
Kid Max <kidmax285@gmail.com>
2025-05-25 11:15:31 +0600

    init

---
+++ database/seeders/RefundDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\Refund\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class RefundDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
