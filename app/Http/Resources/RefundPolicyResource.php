637c6a68adddd1cf5941954fa9a26f2db8a6af9b
Kid Max <kidmax285@gmail.com>
2026-04-03 12:04:36 +0600

    refactor(resource): type refund policy toArray signature

--- app/Http/Resources/RefundPolicyResource.php
+++ app/Http/Resources/RefundPolicyResource.php
@@ -17,9 +17,8 @@ class RefundPolicyResource extends Resource
      * @param  Request  $request
      * @return array
      */
-    public function toArray($request)
+    public function toArray(Request $request): array
     {
-
         return [
             'id' => $this->id,
             'title' => $this->title,
