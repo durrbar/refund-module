756fe48c1404e504d2e3ec3e4abbc80a712d4781
Kid Max <kidmax285@gmail.com>
2026-04-03 12:15:57 +0600

    refactor(resources): type refund toArray request signature

--- app/Http/Resources/RefundResource.php
+++ app/Http/Resources/RefundResource.php
@@ -15,7 +15,7 @@ class RefundResource extends Resource
      * @param  Request  $request
      * @return array
      */
-    public function toArray($request)
+    public function toArray(Request $request): array
     {
         return [
             'id' => $this->id,
